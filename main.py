70712dc7b73283f3555b73946d301a711acd9390
ali entezari <ali_ent12@outlook.com>
2023-01-11 03:56:49 +0330

    updated

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ import usb.core
 import usb.backend.libusb1
 import random
 import time
+import tfest
+from multiprocessing import Process
+from multiprocessing import Manager
 
 libusb1_backend = usb.backend.libusb1.get_backend(find_library=libusb_package.find_library)
 
@@ -28,37 +31,31 @@ memory_dict = {"kpp": 0x20000004, "kip": 0x20000008, "kdp": 0x2000000c, "dp": 0x
                "x_360": 0x2000012c, "x_1024": 0x40010024, "setpoint_x": 0x20000000,
                "man_out": 0x40000034, "control_x": 0x20000028, "auto_mode": 0x20000024}
 
-kpp = 50
-kip = 1
-kdp = 5
-dp = 50
+# stm32 PID vars
+kpp = 50; kip = 1 ;kdp = 5; dp = 50
+kps = 0; kis = 50; kds = 50; ds = 50
 
-kps = 0
-kis = 50
-kds = 50
-ds = 50
+# stm32 data vars
+man_out = 0; x_360 = 0; x_1024 = 0; setpoint_x = 0
+mode = "p"; setpoint_mode = "num"
 
-man_out = 0
+# ploting and curve input vars
+t = np.linspace(0, 2 * np.pi, 500); x_arr = np.zeros_like(t); x_setpoint_arr = np.zeros_like(t)
+T0 = time.time(); T = time.time()
+freq = 2 * np.pi / 100; A = 90
 
-npoles = None
-nzeros = None
-
-x_360 = None
-x_1024 = None
-setpoint_x = 0
-STM_ONLINE = True
-
-mode = "p"
-setpoint_mode = "num"
-
-t = np.linspace(0, 2 * np.pi, 500)
-x_arr = np.zeros_like(t)
-x_setpoint_arr = np.zeros_like(t)
-T0 = time.time()
-T = time.time()
-freq = 2 * np.pi / 100
-A = 90
+# flags
+stm_online = True
 stm32_detected = False
+stupid_counter = 0; stupid_flag = True
+PH_mode = 0
+
+# sampling and estimation vars
+data_input = []; data_output = []; sampling_cnt = 0
+sample_start_T = 0; sample_stop_T = 0
+SAMPLE_NUMS = 500; IMPULSE_WIDTH = 10
+npoles = 2; nzeros = 1
+
 try:
     dev = swd.Swd()
     cm = swd.CortexM(dev)
@@ -89,110 +86,96 @@ class MatplotlibWindow(QMainWindow, Form):
         super(MatplotlibWindow, self).__init__()
         self.setupUi(self)
 
-        self.kpp_ledit.textChanged.connect(self.update_vars)
-        self.kip_ledit.textChanged.connect(self.update_vars)
-        self.kdp_ledit.textChanged.connect(self.update_vars)
-        self.dp_ledit.textChanged.connect(self.update_vars)
-
-        self.kps_ledit.textChanged.connect(self.update_vars)
-        self.kis_ledit.textChanged.connect(self.update_vars)
-        self.kds_ledit.textChanged.connect(self.update_vars)
-        self.ds_ledit.textChanged.connect(self.update_vars)
-
-        self.vertical_slider.valueChanged.connect(self.update_vars)
-
-        self.npole_ledit.textChanged.connect(self.update_vars)
-        self.nzero_ledit.textChanged.connect(self.update_vars)
-
+        self.kpp_ledit.textChanged.connect(self.update_vars);self.kps_ledit.textChanged.connect(self.update_vars)
+        self.kip_ledit.textChanged.connect(self.update_vars);self.kis_ledit.textChanged.connect(self.update_vars)
+        self.kdp_ledit.textChanged.connect(self.update_vars);self.kds_ledit.textChanged.connect(self.update_vars)
+        self.dp_ledit.textChanged.connect(self.update_vars);self.ds_ledit.textChanged.connect(self.update_vars)
+        self.npole_ledit.textChanged.connect(self.update_vars);self.nzero_ledit.textChanged.connect(self.update_vars)
+        self.vertical_slider.valueChanged.connect(self.update_vars);self.man_button.clicked.connect(self.update_vars)
+        self.pos_button.clicked.connect(self.update_vars);self.speed_button.clicked.connect(self.update_vars)
+        self.man_num_button.clicked.connect(self.update_vars);self.man_scroll_button.clicked.connect(self.update_vars)
+        self.step_button.clicked.connect(self.update_vars);self.impulse_button.clicked.connect(self.update_vars)
+        
         self.pos_ledit.textChanged.connect(self.update_vars)
-
-        self.pos_button.clicked.connect(self.update_vars)
-        self.speed_button.clicked.connect(self.update_vars)
-        self.man_button.clicked.connect(self.update_vars)
-        self.man_num_button.clicked.connect(self.update_vars)
-        self.man_scroll_button.clicked.connect(self.update_vars)
-        self.step_button.clicked.connect(self.update_vars)
-        self.impulse_button.clicked.connect(self.update_vars)
         self.sine_button.clicked.connect(self.update_vars)
+        self.estimate_button.clicked.connect(self.update_vars)
 
+        self.kpp_ledit.setText(str(kpp));self.kps_ledit.setText(str(kps))
+        self.kip_ledit.setText(str(kip));self.kis_ledit.setText(str(kis))
+        self.kdp_ledit.setText(str(kdp));self.kds_ledit.setText(str(kds))
+        self.dp_ledit.setText(str(dp));self.ds_ledit.setText(str(ds))   
+        self.pos_ledit.setText(str(0));self.speed_ledit.setText(str(0))
+        self.npole_ledit.setText(str(npoles));self.nzero_ledit.setText(str(nzeros))
 
-        self.kpp_ledit.setText(str(kpp))
-        self.kip_ledit.setText(str(kip))
-        self.kdp_ledit.setText(str(kdp))
-        self.dp_ledit.setText(str(dp))
-        self.kps_ledit.setText(str(kps))
-        self.kis_ledit.setText(str(kis))
-        self.kds_ledit.setText(str(kds))
-        self.ds_ledit.setText(str(ds))
-        self.pos_ledit.setText(str(0))
-        self.speed_ledit.setText(str(0))
-
-        # stm32_detected = True
+        line1_color = "b"
+        line2_color = "r"
+        ax_num_color = "k"
+        ax_line_color = "k"
+        if PH_mode:
+            self.setStyleSheet("color: #ffa31a;  background-color: #292929")
+            line1_color = "#808080"
+            line2_color = "#ffa31a"
+            ax_num_color = "#ffa31a"
+            ax_line_color = "#292929"
+        
+        # global stm32_detected; stm32_detected = True
         if stm32_detected:
             self.status_label.setText("STM32 Detected!")
             self.status_label.setStyleSheet("color: green")
         else:
-            self.status_label.setStyleSheet("color: red;  background-color: black")
+            self.status_label.setStyleSheet("color: black;  background-color: red")
 
         self.thread = None
 
-        self.fig = Figure(frameon=False)
-        self.fig.patch.set_color("w")
+        self.fig = Figure(frameon=False);self.fig.patch.set_color("w")
         self.ax = self.fig.add_subplot(111, frame_on=False)
         self.canvas = FigureCanvas(self.fig)
         self.navi = NavigationToolbar(self.canvas, self)
 
-        l = QVBoxLayout(self.matplotlib_widget)
-        l.addWidget(self.canvas)
-        l.addWidget(self.navi)
+        l = QVBoxLayout(self.matplotlib_widget);l.addWidget(self.canvas);l.addWidget(self.navi)
 
-        x = np.linspace(0, 2 * np.pi, 100)
-        y = np.zeros_like(x)
-        (self.line1,) = self.ax.plot(x, y, "b", lw=2, label="setpoint")
-        (self.line2,) = self.ax.plot(x, y, "r", lw=2, label="sensor")
+        x = np.linspace(0, 2 * np.pi, 100);y = np.zeros_like(x)
+        (self.line1,) = self.ax.plot(x, y, line1_color, lw=2, label="setpoint")
+        (self.line2,) = self.ax.plot(x, y, line2_color, lw=2, label="sensor")
         self.ax.grid()
-        self.ax.set_ylim(-180, 180)
-        self.ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=3, fancybox=True, shadow=True)
-
-        self.thread_plot = PlotThread(self, 0.5)
+        self.ax.set_ylim(-180, 180);self.ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=3, fancybox=True, shadow=True)
+        self.ax.tick_params(axis='x', colors=ax_line_color);[t.set_color(ax_num_color) for t in self.ax.xaxis.get_ticklabels()]
+        self.ax.tick_params(axis='y', colors=ax_line_color);[t.set_color(ax_num_color) for t in self.ax.yaxis.get_ticklabels()]
+        
+        self.thread_plot = PlotThread(self)
         self.thread_plot.update_trigger.connect(self.update_plot)
         self.thread_plot.start()
     
         self.thread = MicroThread(self)
         self.thread.update_trigger.connect(self.update)
         self.thread.start()
+
+        self.thread_estimate = EstimateThread(self)
+        self.thread_estimate.update_trigger.connect(self.update_plot)
     
     def update_vars(self, num):
-        global kpp, kip, kdp, kps, kis, kds, dp, ds, man_out, npoles, nzeros, mode, T0, T, freq, setpoint_mode, setpoint_x
+        global kpp, kip, kdp, kps, kis, kds, dp, ds, man_out, npoles, nzeros, mode, T0, T, freq, setpoint_mode, setpoint_x, data_output, data_input
         if not isinstance(num, bool):
             try:
                 int(num)
             except Exception:
                 return
         if self.sender() == self.kpp_ledit:
-            kpp = int(num)
-            dev.set_mem32(memory_dict["kpp"], kpp)
+            kpp = int(num);dev.set_mem32(memory_dict["kpp"], kpp)
         elif self.sender() == self.kip_ledit:
-            kip = int(num)
-            dev.set_mem32(memory_dict["kip"], kip)
+            kip = int(num);dev.set_mem32(memory_dict["kip"], kip)
         elif self.sender() == self.kdp_ledit:
-            kdp = int(num)
-            dev.set_mem32(memory_dict["kdp"], kdp)
+            kdp = int(num);dev.set_mem32(memory_dict["kdp"], kdp) 
         elif self.sender() == self.dp_ledit:
-            dp = int(num)
-            dev.set_mem32(memory_dict["dp"], dp)
+            dp = int(num);dev.set_mem32(memory_dict["dp"], dp) 
         elif self.sender() == self.kps_ledit:
-            kps = int(num)
-            dev.set_mem32(memory_dict["kps"], kps)
+            kps = int(num);dev.set_mem32(memory_dict["kps"], kps)
         elif self.sender() == self.kis_ledit:
-            kis = int(num)
-            dev.set_mem32(memory_dict["kis"], kis)
+            kis = int(num);dev.set_mem32(memory_dict["kis"], kis)
         elif self.sender() == self.kds_ledit:
-            kds = int(num)
-            dev.set_mem32(memory_dict["kds"], kds)
+            kds = int(num);dev.set_mem32(memory_dict["kds"], kds)
         elif self.sender() == self.ds_ledit:
-            dev.set_mem32(memory_dict["ds"], ds)
-            ds = int(num)
+            ds = int(num);dev.set_mem32(memory_dict["ds"], ds)
         elif self.sender() == self.npole_ledit:
             npoles = int(num)
         elif self.sender() == self.nzero_ledit:
@@ -224,13 +207,12 @@ class MatplotlibWindow(QMainWindow, Form):
         elif self.sender() == self.man_button:
             mode = "m"
             dev.set_mem32(memory_dict["auto_mode"], 0)
-            self.min_label.setText("0 V")
-            self.max_label.setText("6 V")
+            self.min_label.setText("0 V");self.max_label.setText("6 V")
             self.mode_label.setText("Mode: No Controller")
         elif self.sender() == self.man_num_button:
             setpoint_mode = "num"
             if mode == "p" or mode == "m":
-                temp = self.pos_ledit.text()
+                mode = "p";temp = self.pos_ledit.text()
                 self.mode_label.setText("Mode: Position Control")
             elif mode == "s":
                 temp = self.speed_ledit.text()
@@ -244,75 +226,179 @@ class MatplotlibWindow(QMainWindow, Form):
                 setpoint_x = int(temp)
         elif self.sender() == self.man_scroll_button:
             setpoint_mode = "scroll"
-            self.min_label.setText("0 deg")
-            self.max_label.setText("100 deg")
+            self.min_label.setText("0 deg");self.max_label.setText("100 deg")
         elif self.sender() == self.sine_button:
             setpoint_mode = "sine"
-            self.min_label.setText("0.01 Hz")
-            self.max_label.setText("1 Hz")
+            self.min_label.setText("0.01 Hz");self.max_label.setText("1 Hz")
+        elif self.sender() == self.step_button:
+            setpoint_mode = "step"
+            data_input, data_output = [], []
+            dev.set_mem32(memory_dict["auto_mode"], 0)
+        elif self.sender() == self.impulse_button:
+            setpoint_mode = "impulse"
+            data_input, data_output = [], []
+            dev.set_mem32(memory_dict["auto_mode"], 0)
+        elif self.sender() == self.estimate_button:
+            if data_output != []:
+                self.message_label.setText("Estimating Transfer Function...")
+                self.thread_estimate.start()
+            else:
+                self.message_label.setText("Sampling First!")
 
-    def update(self, x):
+    def update(self, x, v):
         self.posval_label.setText(str(x))
+        self.speedval_label.setText(str(v))
+        
+        if not stm32_detected:
+            global stupid_counter, stupid_flag
+            stupid_counter += 1
+            if not stupid_counter%10:
+                if stupid_flag:
+                    self.status_label.setStyleSheet("color: black;  background-color: red")
+                else:
+                    self.status_label.setStyleSheet("color: white;  background-color: red")
+                    # self.status_label.setStyleSheet("color: red;  background-color: #292929")
+                stupid_flag = not stupid_flag
 
-    def update_plot(self):
+    def update_plot(self, message=False):
         self.line1.set_data(t, x_setpoint_arr)
         self.line2.set_data(t, x_arr)
         self.canvas.draw()
-    
+        if message:
+            self.message_label.setText(message)
     
     def closeEvent(self, event):
-        global STM_ONLINE
-        STM_ONLINE = False
+        global stm_online
+        stm_online = False
         cm.reset()
         cm.halt()
 
 
 class PlotThread(QtCore.QThread):
-    update_trigger = QtCore.pyqtSignal()
-
-    def __init__(self, window, decay):
-        QtCore.QThread.__init__(self, parent=window)
-        self.decay = decay
-        self.window = window
-
-    def run(self):
-        global t, x_arr, x_setpoint_arr, setpoint_x, A, T, T0, freq, setpoint_mode
-        while STM_ONLINE:
-            if setpoint_mode == "scroll" and mode != "m":
-                setpoint_x = man_out
-            elif setpoint_mode == "sine" and mode != "m":
-                T = time.time() - T0
-                setpoint_x = A * np.sin(freq * T)
-            
-            x_360 = dev.get_mem32(memory_dict["x_360"])
-            if x_360 > 180:
-                x_360 -= 360
-            dev.set_mem32(memory_dict["setpoint_x"], setpoint_x)
-            
-            x_arr = np.roll(x_arr, 1)
-            x_setpoint_arr = np.roll(x_setpoint_arr, 1)
-            x_setpoint_arr[0] = setpoint_x
-            x_arr[0] = x_360
-            self.update_trigger.emit()
-
-class MicroThread(QtCore.QThread):
-    update_trigger = QtCore.pyqtSignal(int)
+    update_trigger = QtCore.pyqtSignal(str)
 
     def __init__(self, window):
         QtCore.QThread.__init__(self, parent=window)
         self.window = window
 
     def run(self):
-        global kpp, kip, kdp, kps, kis, kds, dp, ds, man_out, npoles, nzeros
-        while STM_ONLINE:
+        global t, x_arr, x_setpoint_arr, setpoint_x, A, T, T0, freq, setpoint_mode, sampling_cnt, sample_start_T, sample_stop_T
+        while stm_online:
+            if setpoint_mode == "scroll" and mode != "m":
+                setpoint_x = man_out
+            elif setpoint_mode == "sine" and mode != "m":
+                T = time.time() - T0
+                setpoint_x = A * np.sin(freq * T)
+            elif setpoint_mode == "step":
+                if sampling_cnt == 0:
+                    sample_start_T = time.time()
+                
+                if sampling_cnt < (SAMPLE_NUMS * 0.01):
+                    setpoint_x = 0
+                    dev.set_mem32(memory_dict["man_out"], 0)
+                elif sampling_cnt < SAMPLE_NUMS:
+                    setpoint_x = 100
+                    dev.set_mem32(memory_dict["man_out"], 420000)
+                sampling_cnt += 1
+            elif setpoint_mode == "impulse":
+                if sampling_cnt == 0:
+                    sample_start_T = time.time()
+                
+                if (SAMPLE_NUMS * 0.01) < sampling_cnt < (SAMPLE_NUMS * 0.01 + IMPULSE_WIDTH):
+                    setpoint_x = 100
+                    dev.set_mem32(memory_dict["man_out"], 420000)
+                else:
+                    setpoint_x = 0
+                    dev.set_mem32(memory_dict["man_out"], 0)
+                sampling_cnt += 1
+            
             x_360 = dev.get_mem32(memory_dict["x_360"])
             if x_360 > 180:
                 x_360 -= 360
-            self.update_trigger.emit(int(x_360))
+            dev.set_mem32(memory_dict["setpoint_x"], setpoint_x)
+            
+            message = ""
+            if setpoint_mode == "step" or setpoint_mode == "impulse":
+                data_output.append(x_360)
+                data_input.append(setpoint_x)
+                message = f"Sampling... {sampling_cnt}/{SAMPLE_NUMS}"
+                if sampling_cnt == SAMPLE_NUMS:
+                    sample_stop_T = time.time()
+                    setpoint_mode = "num"
+                    sampling_cnt = 0
+                    message = f"Sampling finished"
+            
+            x_arr = np.roll(x_arr, 1)
+            x_setpoint_arr = np.roll(x_setpoint_arr, 1)
+            x_setpoint_arr[0] = setpoint_x
+            x_arr[0] = x_360
+            self.update_trigger.emit(message)
 
+class MicroThread(QtCore.QThread):
+    update_trigger = QtCore.pyqtSignal(int, int)
+
+    def __init__(self, window):
+        QtCore.QThread.__init__(self, parent=window)
+        self.window = window
+
+    def run(self):
+        global kpp, kip, kdp, kps, kis, kds, dp, ds, man_out, npoles, nzeros, x_1024
+        t_speed_meter = time.time()
+        x_1024_old = 0
+        while stm_online:
+            x_360 = dev.get_mem32(memory_dict["x_360"])
+            x_1024_old = x_1024
+            x_1024 = dev.get_mem32(memory_dict["x_1024"])
+            t_temp = time.time()
+            v = (x_1024 - x_1024_old) / (t_temp - t_speed_meter) / 1024 * 360
+            t_speed_meter = t_temp
+            if x_360 > 180:
+                x_360 -= 360
+            self.update_trigger.emit(int(x_360), int(v))
+
+class EstimateThread(QtCore.QThread):
+    update_trigger = QtCore.pyqtSignal(str)
+
+    def __init__(self, window):
+        QtCore.QThread.__init__(self, parent=window)
+        self.window = window
+
+    def run(self):
+        global data_input, data_output, npoles, nzeros, return_dict
+        input_arr = np.array(data_input)
+        output_arr = np.array(data_output)
+        t_tot = sample_stop_T - sample_start_T
+        proc = Process(target=estimate_tf, args=(input_arr, output_arr, t_tot, return_dict))
+        return_dict["message"] = ""
+        proc.start()
+        proc.join()
+        self.update_trigger.emit(return_dict["message"])
+        
+def estimate_tf(input_arr, output_arr, t_tot, return_dict):
+    te = tfest.tfest(input_arr, output_arr)
+    te.estimate(nzeros, npoles, time=t_tot)
+    tf = te.get_transfer_function()
+    num = tf.num.tolist()
+    den = tf.den.tolist()
+    message = ""
+    for i, c in enumerate(num):
+        if i==0:
+            message += f"{c:.2f}"
+        else:
+            message += f"+{c:.2f}S^{i}"
+    message += "\n"+ npoles*20*"-" +"\n"
+    for i, c in enumerate(den):
+        if i==0:
+            message += f"{c:.2f}"
+        else:
+            message += f"+{c:.2f}S^{i}"
+    return_dict["message"] = message
+    print("TF:\n", message)
 
 
 if __name__ == "__main__":
+    manager = Manager()
+    return_dict = manager.dict()
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
     w = MatplotlibWindow()
